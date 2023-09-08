66826fded9383adfb399bc69ebeb7e4533a6cac9
AnhTuan <tstnatuan@gmail.com>
2023-09-08 14:59:31 +0700

    first commit

---
+++ ConsoleApp233/ConsoleApp233/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HelloWorldApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** My First C$ App*****");
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
